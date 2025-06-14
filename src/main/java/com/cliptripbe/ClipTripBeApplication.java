37e367c299204b8fc16fc365595058a0358280ea
TaetaetaE01 <gogo589@naver.com>
2025-06-14 20:38:07 +0900

    init: project 세팅

---
+++ src/main/java/com/cliptripbe/ClipTripBeApplication.java
@@ -0,0 +1,13 @@
+package com.cliptripbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipTripBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClipTripBeApplication.class, args);
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/cliptripbe/ClipTripBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliptripbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipTripBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
