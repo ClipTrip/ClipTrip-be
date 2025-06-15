8b3c41745bde33f8f71eea648be0d0d15653aa4d
TaetaetaE01 <gogo589@naver.com>
2025-06-16 00:31:32 +0900

    chore: docker-compose 로컬 db 설정

--- src/main/java/com/cliptripbe/ClipTripBeApplication.java
+++ src/main/java/com/cliptripbe/ClipTripBeApplication.java
@@ -8,6 +8,5 @@ public class ClipTripBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClipTripBeApplication.class, args);
-        System.out.println("Hello World!");
     }
 }
