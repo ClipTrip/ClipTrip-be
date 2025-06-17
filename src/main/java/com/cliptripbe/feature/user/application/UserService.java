927620aa9802d815c41e36f5ef4ab65912a21658
TaetaetaE01 <gogo589@naver.com>
2025-06-18 01:43:26 +0900

    chore: User 패키지구조 설계

---
+++ src/main/java/com/cliptripbe/feature/user/application/UserService.java
@@ -0,0 +1,10 @@
+package com.cliptripbe.feature.user.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
---
+++ src/main/java/com/cliptripbe/feature/user/infrastructure/UserRepository.java
@@ -0,0 +1,8 @@
+package com.cliptripbe.feature.user.infrastructure;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+
+}
