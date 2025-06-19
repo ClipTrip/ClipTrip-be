0e47180b9da3ffb37d149ab0c54390d974fcb1f5
TaetaetaE01 <gogo589@naver.com>
2025-06-20 01:27:42 +0900

    feat: video 엔티티 생ㅅ어

--- src/main/java/com/cliptripbe/feature/video/domain/Video.java
+++ src/main/java/com/cliptripbe/feature/video/domain/Video.java
@@ -1,5 +1,45 @@
 package com.cliptripbe.feature.video.domain;
 
-public class Video {
+import com.cliptripbe.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Entity
+@Table(name = "videos")
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Video extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Column(name = "youtubeVideoId", nullable = false)
+    private String youtubeVideoId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "summaryKo", nullable = false)
+    private String summaryKo;
+
+    @Column(name = "summaryTranslated")
+    private String summaryTranslated;
+
+    @Column(name = "translatedLangCode")
+    private String translatedLangCode;
 }
