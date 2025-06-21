43e6588eea8e43c4bc8a1aadbbc56cde54c94012
0702Yoon <gkstkddbs99@naver.com>
2025-06-21 21:12:50 +0900

    [Feat] S3 연결 및 데이터를 읽는 로직 구현

---
+++ src/main/java/com/cliptripbe/infrastructure/file/reader/FileKind.java
@@ -0,0 +1,11 @@
+package com.cliptripbe.infrastructure.file.reader;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum FileKind {
+    BF_CULTURE_TOURISM("한국문화정보원_전국 배리어프리 문화예술관광지_20221125.csv");
+    final String fileName;
+}
