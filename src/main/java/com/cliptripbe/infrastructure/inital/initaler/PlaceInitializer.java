fa4834943cae6fc173614b34132bac5b0aa6a204
0702Yoon <gkstkddbs99@naver.com>
2025-07-01 19:09:49 +0900

    [feat] 서버 값 초기화하는 로직 추가

---
+++ src/main/java/com/cliptripbe/infrastructure/inital/initaler/PlaceInitializer.java
@@ -0,0 +1,60 @@
+package com.cliptripbe.infrastructure.inital.initaler;
+
+
+import static com.cliptripbe.infrastructure.file.FileKind.BF_CULTURE_TOURISM;
+
+import com.cliptripbe.feature.place.application.PlaceMapper;
+import com.cliptripbe.feature.place.domain.entity.Place;
+import com.cliptripbe.feature.place.infrastructure.PlaceRepository;
+import com.cliptripbe.infrastructure.file.FileKind;
+import com.cliptripbe.infrastructure.s3.S3FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PlaceInitializer {
+
+    private final PlaceRepository placeRepository;
+
+    private final S3FileReader s3FileReader;
+
+    private final PlaceMapper placeMapper;
+
+    public void registerCulturePlace() {
+        List<Place> placeList = new ArrayList<>();
+        try (BufferedReader br = s3FileReader.readCsv(BF_CULTURE_TOURISM.getFileName())) {
+            String line;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                Place place = placeMapper.mapPlaceCulture(line);
+                placeList.add(place);
+            }
+            placeRepository.saveAll(placeList);
+        } catch (IOException e) {
+            // 예외 처리
+            e.printStackTrace();
+        }
+    }
+
+    public void registerStoragePlace() {
+        List<Place> placeList = new ArrayList<>();
+        try (BufferedReader br = s3FileReader.readCsv(
+            FileKind.RB_TURIST_THNG_DPSTRY_LCINFO.getFileName())) {
+            String line;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                Place place = placeMapper.mapPlaceThng(line);
+                placeList.add(place);
+            }
+            placeRepository.saveAll(placeList);
+        } catch (IOException e) {
+            // 예외 처리
+            e.printStackTrace();
+        }
+    }
+}
