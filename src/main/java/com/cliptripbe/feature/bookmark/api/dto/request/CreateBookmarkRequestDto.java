4a7ac286277cb4bc09eb9d71aa1082a63a7337d5
0702Yoon <gkstkddbs99@naver.com>
2025-07-01 22:28:26 +0900

    [refactor] Bookmark add, delete 대신 Put을 활용해 추가 및 삭제 하도록 변경

--- src/main/java/com/cliptripbe/feature/bookmark/api/dto/request/CreateBookmarkRequestDto.java
+++ src/main/java/com/cliptripbe/feature/bookmark/api/dto/request/CreateBookmarkRequestDto.java
@@ -1,12 +1,8 @@
 package com.cliptripbe.feature.bookmark.api.dto.request;
 
-import com.cliptripbe.feature.place.api.dto.PlaceInfoRequestDto;
-import java.util.List;
-
 public record CreateBookmarkRequestDto(
     String bookmarkName,
-    String description,
-    List<PlaceInfoRequestDto> placeInfoRequestDtos
+    String description
 ) {
 
 }
