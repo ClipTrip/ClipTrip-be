d081e8ef0bf34e78a604d9b287c0e71604c1b2aa
0702Yoon <gkstkddbs99@naver.com>
2025-07-01 19:10:09 +0900

    [feat] 서버 값 초기화하는 로직 추가

--- src/main/java/com/cliptripbe/feature/place/infrastructure/PlaceRepository.java
+++ src/main/java/com/cliptripbe/feature/place/infrastructure/PlaceRepository.java
@@ -1,6 +1,8 @@
 package com.cliptripbe.feature.place.infrastructure;
 
 import com.cliptripbe.feature.place.domain.entity.Place;
+import com.cliptripbe.feature.place.domain.type.PlaceType;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +15,10 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
         @Param("name") String name,
         @Param("roadAddress") String roadAddress
     );
+
+    @Query("SELECT p FROM Place p WHERE p.placeType = :placeType AND p.address.roadAddress LIKE %:region%")
+    List<Place> findByRegionAndPlaceType(@Param("region") String region,
+        @Param("placeType") PlaceType placeType);
+
+
 }
