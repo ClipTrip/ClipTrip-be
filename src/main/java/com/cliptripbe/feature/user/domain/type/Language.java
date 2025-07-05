c1dc6066ee811b26bc75e37621b82add0259d45b
0702Yoon <gkstkddbs99@naver.com>
2025-07-05 17:46:30 +0900

    [feat] 장소 조회시 영문으로 된 장소 없을 경우 디비에 추가하는 로직 구현

--- src/main/java/com/cliptripbe/feature/user/domain/type/Language.java
+++ src/main/java/com/cliptripbe/feature/user/domain/type/Language.java
@@ -1,6 +1,12 @@
 package com.cliptripbe.feature.user.domain.type;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Language {
-    KOREAN,
-    ENGLISH;
+    KOREAN("한국어"),
+    ENGLISH("ENGLISH");
+    private final String name;
 }
