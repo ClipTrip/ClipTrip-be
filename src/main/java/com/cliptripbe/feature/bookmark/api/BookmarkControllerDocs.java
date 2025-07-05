ced373a2e871dc7a237f9ea25d6faa1076a07ef1
TaetaetaE01 <gogo589@naver.com>
2025-07-05 18:52:52 +0900

    refactor: 북마크 스웨거 인터페이스 응답 값 추가

--- src/main/java/com/cliptripbe/feature/bookmark/api/BookmarkControllerDocs.java
+++ src/main/java/com/cliptripbe/feature/bookmark/api/BookmarkControllerDocs.java
@@ -2,37 +2,40 @@ package com.cliptripbe.feature.bookmark.api;
 
 import com.cliptripbe.feature.bookmark.api.dto.request.CreateBookmarkRequestDto;
 import com.cliptripbe.feature.bookmark.api.dto.request.UpdateBookmarkRequestDto;
+import com.cliptripbe.feature.bookmark.api.dto.response.BookmarkInfoResponseDto;
+import com.cliptripbe.feature.bookmark.api.dto.response.BookmarkListResponseDto;
 import com.cliptripbe.feature.place.api.dto.PlaceInfoRequestDto;
 import com.cliptripbe.global.auth.security.CustomerDetails;
 import com.cliptripbe.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 
 @Tag(name = "북마크 관련 API")
 public interface BookmarkControllerDocs {
 
     @Operation(summary = "북마크 만들기, \n로그인 필요")
-    ApiResponse<?> createBookmark(CustomerDetails customerDetails,
+    ApiResponse<Long> createBookmark(CustomerDetails customerDetails,
         CreateBookmarkRequestDto CreateBookmarkRequestDto);
 
     @Operation(summary = "북마크 수정하기, \n로그인 필요,put 메서드")
-    ApiResponse<?> updateBookmark(
+    ApiResponse<Long> updateBookmark(
         Long bookmarkId,
         UpdateBookmarkRequestDto updateBookmarkRequestDto
     );
 
     @Operation(summary = "북마크에 하나의 장소 추가하기, \n로그인 필요")
-    ApiResponse<?> addBookmark(
+    ApiResponse<Long> addBookmark(
         CustomerDetails customerDetails,
         Long bookmarkId,
         PlaceInfoRequestDto placeInfoRequestDto
     );
 
     @Operation(summary = "유저 북마크 전체 조회하기, \n로그인 필요")
-    ApiResponse<?> getUserBookmark(CustomerDetails customerDetails);
+    ApiResponse<List<BookmarkListResponseDto>> getUserBookmark(CustomerDetails customerDetails);
 
     @Operation(summary = "북마크안의 장소 리스트 조회하기, \n로그인 필요")
-    ApiResponse<?> getBookmarkInfo(Long bookmarkId);
+    ApiResponse<BookmarkInfoResponseDto> getBookmarkInfo(Long bookmarkId);
 
     @Operation(summary = "북마크 삭제하기, \n로그인 필요")
     ApiResponse<?> deleteBookmark(
--- src/main/java/com/cliptripbe/feature/place/api/PlaceController.java
+++ src/main/java/com/cliptripbe/feature/place/api/PlaceController.java
@@ -83,24 +83,4 @@ public class PlaceController implements PlaceControllerDocs {
         return ApiResponse.success(SuccessType.SUCCESS, places);
     }
 
-//    @Operation(
-//        summary = "도로명 주소로 장소 사진 조회",
-//        description = "입력한 도로명 주소로 첫 번째 장소를 검색하여, 그 사진을 JPEG 바이너리로 반환합니다."
-//    )
-//    @GetMapping(
-//        path = "/photo/by-address",
-//        produces = MediaType.IMAGE_JPEG_VALUE     // <- 여기서 이미지 타입 명시
-//    )
-//    public Mono<ResponseEntity<ByteArrayResource>> getPhotoByAddress(
-//        @RequestParam("address") String address
-//    ) {
-//        return googlePlacesService.getPhotoByAddress(address)
-//            .map(bytes -> {
-//                var resource = new ByteArrayResource(bytes);
-//                return ResponseEntity.ok()
-//                    .contentLength(bytes.length)
-//                    .contentType(MediaType.IMAGE_JPEG)
-//                    .body(resource);
-//            });
-//    }
 }
