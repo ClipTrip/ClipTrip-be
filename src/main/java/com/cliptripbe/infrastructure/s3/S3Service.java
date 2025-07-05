91cdfecac820af3f678b6d428e3fd1f904dfc45f
TaetaetaE01 <gogo589@naver.com>
2025-07-05 17:31:29 +0900

    feat: s3 file upload 구현

--- src/main/java/com/cliptripbe/infrastructure/s3/S3Service.java
+++ src/main/java/com/cliptripbe/infrastructure/s3/S3Service.java
@@ -7,16 +7,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 
 @Component
 @RequiredArgsConstructor
-public class S3FileReader {
+public class S3Service {
 
     private final S3Client s3Client;
+    private final Region awsRegion;
 
     @Value("${s3.bucket}")
     private String bucket;
@@ -29,5 +34,21 @@ public class S3FileReader {
 
         ResponseInputStream<GetObjectResponse> s3Stream = s3Client.getObject(request);
         return new BufferedReader(new InputStreamReader(s3Stream, StandardCharsets.UTF_8));
+    }
+
+    public String upload(String key, byte[] data) {
+        PutObjectRequest req = PutObjectRequest.builder()
+            .bucket(bucket)
+            .key(key)
+            .acl(ObjectCannedACL.PUBLIC_READ)
+            .contentType("image/jpeg")
+            .build();
+
+        s3Client.putObject(req, RequestBody.fromBytes(data));
+        return String.format("https://%s.s3.%s.amazonaws.com/%s",
+            bucket,
+            awsRegion.id(),
+            key
+        );
     }
 }
