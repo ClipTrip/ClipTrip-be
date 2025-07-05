1fc1d2a78312e1ede49b2884426d058975258354
0702Yoon <gkstkddbs99@naver.com>
2025-07-05 18:12:56 +0900

    [feat] 장소 조회시 유저가 외국인일 경우 장소명과 도로명을 영문으로 반환하도록 구현

--- src/main/java/com/cliptripbe/feature/place/api/dto/response/PlaceResponseDto.java
+++ src/main/java/com/cliptripbe/feature/place/api/dto/response/PlaceResponseDto.java
@@ -1,6 +1,7 @@
 package com.cliptripbe.feature.place.api.dto.response;
 
 import com.cliptripbe.feature.place.domain.entity.Place;
+import com.cliptripbe.feature.place.domain.entity.PlaceTranslation;
 import com.cliptripbe.feature.place.domain.type.AccessibilityFeature;
 import java.util.List;
 import lombok.Builder;
@@ -24,7 +25,25 @@ public record PlaceResponseDto(
             .placeName(place.getName())
             .roadAddress(place.getAddress().roadAddress())
             .phone(place.getPhoneNumber())
-            .type(place.getPlaceType().getDisplayName())
+            .type(place.getPlaceType().getKorName())
+            .longitude(place.getAddress().longitude())
+            .latitude(place.getAddress().latitude())
+            .accessibilityFeatures(place.getAccessibilityFeatures())
+            .bookmarked(bookmarked)
+            .build();
+    }
+
+    public static PlaceResponseDto of(
+        Place place,
+        Boolean bookmarked,
+        PlaceTranslation placeTranslation
+    ) {
+        return PlaceResponseDto.builder()
+            .placeId(place.getId())
+            .placeName(placeTranslation.getName())
+            .roadAddress(placeTranslation.getRoadAddress())
+            .phone(place.getPhoneNumber())
+            .type(place.getPlaceType().getEngName())
             .longitude(place.getAddress().longitude())
             .latitude(place.getAddress().latitude())
             .accessibilityFeatures(place.getAccessibilityFeatures())
