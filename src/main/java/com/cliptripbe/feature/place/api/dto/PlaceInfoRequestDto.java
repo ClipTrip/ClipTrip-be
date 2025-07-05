ada7986bb395823d87aabe1f7e111fb3982e3fdc
0702Yoon <gkstkddbs99@naver.com>
2025-07-05 16:05:32 +0900

    [refactor] PlaceInfoRequestDto에 PlaceType 받도록 수정

--- src/main/java/com/cliptripbe/feature/place/api/dto/PlaceInfoRequestDto.java
+++ src/main/java/com/cliptripbe/feature/place/api/dto/PlaceInfoRequestDto.java
@@ -1,5 +1,6 @@
 package com.cliptripbe.feature.place.api.dto;
 
+import com.cliptripbe.feature.place.domain.type.PlaceType;
 import com.cliptripbe.feature.place.domain.vo.Address;
 import jakarta.validation.constraints.Pattern;
 
@@ -7,7 +8,9 @@ public record PlaceInfoRequestDto(
     Address address,
     String placeName,
     @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호는 000-0000-0000 형식이어야 합니다.")
-    String phoneNumber
+    String phoneNumber,
+    PlaceType type
+    
 ) {
 
 }
