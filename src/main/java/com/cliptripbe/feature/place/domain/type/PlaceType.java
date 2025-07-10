6b80e31debd4783bc6500709fae53c4aff744dc4
TaetaetaE01 <gogo589@naver.com>
2025-07-10 21:17:21 +0900

    refactor: place type 추가

--- src/main/java/com/cliptripbe/feature/place/domain/type/PlaceType.java
+++ src/main/java/com/cliptripbe/feature/place/domain/type/PlaceType.java
@@ -9,18 +9,27 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum PlaceType {
 
-    LUGGAGE_STORAGE(" ", "Luggage Storage", "물품보관소"),
+    LARGE_MART("MT1", "Large Mart", "대형마트"),
+    CONVENIENCE_STORE("CS2", "Convenience Store", "편의점"),
+    CHILD_CARE("PS3", "Child Care & Kindergarten", "어린이집, 유치원"),
+    SCHOOL("SC4", "School", "학교"),
+    ACADEMY("AC5", "Private Academy", "학원"),
+    PARKING_LOT("PK6", "Parking Lot", "주차장"),
+    GAS_STATION("OL7", "Gas Station", "주유소, 충전소"),
+    SUBWAY_STATION("SW8", "Subway Station", "지하철역"),
+    BANK("BK9", "Bank", "은행"),
     CULTURAL_FACILITY("CT1", "Cultural Facility", "문화시설"),
+    REAL_ESTATE_AGENCY("AG2", "Real Estate Agency", "중개업소"),
+    PUBLIC_INSTITUTION("PO3", "Public Institution", "공공기관"),
     TOURIST_ATTRACTION("AT4", "Tourist Attraction", "관광명소"),
-
     ACCOMMODATION("AD5", "Accommodation", "숙박"),
     RESTAURANT("FD6", "Restaurant", "음식점"),
     CAFE("CE7", "Cafe", "카페"),
-    PARKING_LOT("PK6", "Parking Lot", "주차장"),
-    PUBLIC_INSTITUTION("PO3", "Public Institution", "공공기관"),
-    TRANSPORTATION("SW8", "Transportation", "교통시설"),
     HOSPITAL("HP8", "Hospital", "병원"),
+    PHARMACY("PM9", "Pharmacy", "약국"),
 
+    // 기타
+    LUGGAGE_STORAGE("LS1", "Luggage Storage", "물품보관소"),
     ETC("ETC", "Others", "기타");
 
     private final String code;
