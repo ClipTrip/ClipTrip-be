8438703b04a101a6cc7cb9226bf0b4a266a3761b
TaetaetaE01 <gogo589@naver.com>
2025-07-17 14:42:17 +0900

    refactor: 외부 api 호출시 트랜잭션 분리

--- src/main/java/com/cliptripbe/feature/place/application/PlaceTranslationService.java
+++ src/main/java/com/cliptripbe/feature/place/application/PlaceTranslationService.java
@@ -18,14 +18,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class PlaceTranslationService {
 
     private final ChatGPTService chatGPTService;
     private final PlaceTranslationRepository placeTranslationRepository;
 
-
+    @Transactional
     public void registerPlace(Place place) {
         placeTranslationRepository.findByPlaceAndLanguage(place, ENGLISH)
             .orElseGet(() -> {
