8be572109239167e31a2d9db298a04faa9e52d08
0702Yoon <gkstkddbs99@naver.com>
2025-07-18 00:12:10 +0900

    [refactor] PlaceInfoRequestDto의 중첩 클래스 제거

--- src/main/java/com/cliptripbe/feature/place/api/dto/PlaceInfoRequestDto.java
+++ src/main/java/com/cliptripbe/feature/place/api/dto/PlaceInfoRequestDto.java
@@ -5,12 +5,17 @@ import com.cliptripbe.feature.place.domain.vo.Address;
 import jakarta.validation.constraints.Pattern;
 
 public record PlaceInfoRequestDto(
-    Address address,
+    double latitude,
+    double longitude,
+    String roadAddress,
     String placeName,
     @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호는 000-0000-0000 형식이어야 합니다.")
     String phoneNumber,
     PlaceType type
-    
+
 ) {
 
+    public Address getAddress() {
+        return new Address(latitude, longitude, roadAddress);
+    }
 }
