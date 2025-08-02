dca8887e9bc845fc37d7094a443340a8dbfd019b
0702Yoon <gkstkddbs99@naver.com>
2025-08-02 23:19:28 +0900

    refactor: 기본으로 제공하던 북마크가 광고를 통해 받도록 하기에 제거.

--- src/main/java/com/cliptripbe/feature/user/application/UserService.java
+++ src/main/java/com/cliptripbe/feature/user/application/UserService.java
@@ -1,9 +1,5 @@
 package com.cliptripbe.feature.user.application;
 
-import com.cliptripbe.feature.bookmark.domain.entity.Bookmark;
-import com.cliptripbe.feature.bookmark.domain.entity.BookmarkPlace;
-import com.cliptripbe.feature.bookmark.domain.service.BookmarkFinder;
-import com.cliptripbe.feature.bookmark.infrastructure.BookmarkRepository;
 import com.cliptripbe.feature.user.domain.entity.User;
 import com.cliptripbe.feature.user.domain.service.UserChecker;
 import com.cliptripbe.feature.user.domain.service.UserLoader;
@@ -14,7 +10,6 @@ import com.cliptripbe.feature.user.dto.response.UserLoginResponse;
 import com.cliptripbe.feature.user.infrastructure.UserRepository;
 import com.cliptripbe.global.auth.service.AuthService;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -32,8 +27,6 @@ public class UserService {
     private final UserChecker userChecker;
     private final UserRepository userRepository;
     private final UserLoader userLoader;
-    private final BookmarkFinder bookmarkFinder;
-    private final BookmarkRepository bookmarkRepository;
 
     @Transactional
     public UserInfoResponse signUp(UserSignUpRequest signUpDto) {
@@ -41,30 +34,6 @@ public class UserService {
         String encodePassword = passwordEncoder.encode(signUpDto.password());
         User user = signUpDto.toEntity(encodePassword);
         userRepository.save(user);
-
-        List<Bookmark> defaultBookmarks = bookmarkFinder.getDefaultBookmarksExcludingStorageSeoul();
-
-        List<Bookmark> newUserBookmarks = new ArrayList<>();
-
-        for (Bookmark bm : defaultBookmarks) {
-            Bookmark copiedBookmark = Bookmark
-                .builder()
-                .name(bm.getName())
-                .user(user)
-                .description(bm.getDescription())
-                .build();
-            if (bm.getBookmarkPlaces() != null) {
-                for (BookmarkPlace originalBp : bm.getBookmarkPlaces()) {
-                    BookmarkPlace newBookmarkPlace = BookmarkPlace.builder()
-                        .bookmark(copiedBookmark)
-                        .place(originalBp.getPlace())
-                        .build();
-                    copiedBookmark.addBookmarkPlace(newBookmarkPlace);
-                }
-            }
-            newUserBookmarks.add(copiedBookmark); // Add the fully constructed copied bookmark
-        }
-        bookmarkRepository.saveAll(newUserBookmarks);
         return UserInfoResponse.from(user);
     }
 
