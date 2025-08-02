0205eada4bb52913a78860a0c126b27daa51c397
TaetaetaE01 <gogo589@naver.com>
2025-08-02 15:24:06 +0900

    hotfix: git add 누락

---
+++ src/main/java/com/cliptripbe/infrastructure/adapter/out/kakao/dto/KakaoMapResponse.java
@@ -0,0 +1,31 @@
+package com.cliptripbe.infrastructure.adapter.out.kakao.dto;
+
+import java.util.List;
+
+public record KakaoMapResponse(
+    Meta meta,
+    List<Document> documents
+) {
+
+    public record Meta(
+        int total_count,
+        int pageable_count,
+        boolean is_end
+    ) {
+
+    }
+
+    public record Document(
+        String place_name,
+        String address_name,
+        String road_address_name,
+        String id,
+        String phone,
+        String category_name,
+        String category_group_code,
+        String x,
+        String y
+    ) {
+
+    }
+}
