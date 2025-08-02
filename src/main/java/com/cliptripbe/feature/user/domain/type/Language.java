82bbf878a7edb7cbb882548e9423f84d357e95d1
0702Yoon <gkstkddbs99@naver.com>
2025-08-02 23:19:12 +0900

    refactor: 회원가입시 받는 국적의 종류 추가

--- src/main/java/com/cliptripbe/feature/user/domain/type/Language.java
+++ src/main/java/com/cliptripbe/feature/user/domain/type/Language.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Language {
     KOREAN("한국어"),
-    ENGLISH("ENGLISH");
+    ENGLISH("영어"),
+    CHINESE("중국어"),
+    JAPANESE("일본어");
 
     private final String name;
 }
