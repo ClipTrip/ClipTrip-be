365f2c574ec1d4298958c20e538faf7056c27242
0702Yoon <gkstkddbs99@naver.com>
2025-08-11 12:59:21 +0900

    refactor: age validation 조건 추가

--- src/main/java/com/cliptripbe/feature/user/dto/request/UserSignUpRequest.java
+++ src/main/java/com/cliptripbe/feature/user/dto/request/UserSignUpRequest.java
@@ -6,13 +6,15 @@ import com.cliptripbe.feature.user.domain.type.CountryCode;
 import com.cliptripbe.feature.user.domain.type.Gender;
 import com.cliptripbe.feature.user.domain.type.Language;
 import com.cliptripbe.feature.user.domain.type.Role;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record UserSignUpRequest(
     @NotNull String email,
     @NotNull String password,
     @NotNull Gender gender,
-    @NotNull Integer age,
+    @NotNull @Min(0) @Max(120) Integer age,
     @NotNull Language language,
     @NotNull CountryCode countryCode
 ) {
