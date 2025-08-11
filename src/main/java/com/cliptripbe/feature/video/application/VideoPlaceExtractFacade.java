cab1fdf3f01e0a25ed7033e4c119fe7e3bdf0f33
0702Yoon <gkstkddbs99@naver.com>
2025-08-11 17:37:31 +0900

    패키지 이동

--- src/main/java/com/cliptripbe/feature/video/application/VideoPlaceExtractFacade.java
+++ src/main/java/com/cliptripbe/feature/video/application/VideoPlaceExtractFacade.java
@@ -1,22 +1,22 @@
 package com.cliptripbe.feature.video.application;
 
-import com.cliptripbe.feature.place.dto.PlaceDto;
 import com.cliptripbe.feature.place.application.PlaceService;
-import com.cliptripbe.feature.place.application.PlaceTranslationService;
 import com.cliptripbe.feature.place.domain.entity.Place;
+import com.cliptripbe.feature.place.dto.PlaceDto;
 import com.cliptripbe.feature.schedule.application.ScheduleService;
 import com.cliptripbe.feature.schedule.domain.entity.Schedule;
+import com.cliptripbe.feature.translate.application.PlaceTranslationService;
 import com.cliptripbe.feature.user.domain.entity.User;
 import com.cliptripbe.feature.user.domain.type.Language;
 import com.cliptripbe.feature.video.domain.entity.Video;
 import com.cliptripbe.feature.video.dto.request.ExtractPlaceRequest;
 import com.cliptripbe.feature.video.dto.response.VideoScheduleResponse;
 import com.cliptripbe.global.util.ChatGPTUtils;
-import com.cliptripbe.infrastructure.adapter.out.caption.dto.CaptionResponse;
-import com.cliptripbe.infrastructure.port.kakao.PlaceSearchPort;
 import com.cliptripbe.global.util.prompt.PromptUtils;
 import com.cliptripbe.global.util.prompt.type.PromptType;
+import com.cliptripbe.infrastructure.adapter.out.caption.dto.CaptionResponse;
 import com.cliptripbe.infrastructure.port.caption.VideoContentExtractPort;
+import com.cliptripbe.infrastructure.port.kakao.PlaceSearchPort;
 import com.cliptripbe.infrastructure.port.openai.AiTextProcessorPort;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
