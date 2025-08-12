2cb84dab64f3c8d88e38d617327d830e3b215fef
0702Yoon <gkstkddbs99@naver.com>
2025-08-12 15:42:59 +0900

    refactor: 범용성 좋게 매개변수 변경

--- src/main/java/com/cliptripbe/global/util/CacheUtils.java
+++ src/main/java/com/cliptripbe/global/util/CacheUtils.java
@@ -1,12 +1,13 @@
 package com.cliptripbe.global.util;
 
-import com.cliptripbe.feature.place.dto.PlaceDto;
 import com.cliptripbe.feature.user.domain.type.Language;
 
 public class CacheUtils {
 
-    public static String createTranslatedPlaceKey(PlaceDto placeDto, Language userLanguage) {
-        return String.format("%s %s %s", placeDto.placeName(), placeDto.roadAddress(),
-            userLanguage);
+    public static String createTranslatedPlaceKey(
+        String placeName,
+        String roadAddress,
+        Language userLanguage) {
+        return String.format("%s %s %s", placeName, roadAddress, userLanguage);
     }
 }
