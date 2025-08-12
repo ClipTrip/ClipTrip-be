2d8ba71a385616cc3dc866a4b5477e094bc4d398
0702Yoon <gkstkddbs99@naver.com>
2025-08-12 15:43:50 +0900

    PlaceInfoWithTranslation -> TranslatedPlaceAddress 사용으로 인한 변경점

--- src/main/java/com/cliptripbe/feature/translate/dto/TranslationSplitResult.java
+++ src/main/java/com/cliptripbe/feature/translate/dto/TranslationSplitResult.java
@@ -1,34 +1,18 @@
 package com.cliptripbe.feature.translate.dto;
 
-import com.cliptripbe.feature.place.domain.vo.PlaceInfoWithTranslation;
-import com.cliptripbe.feature.place.domain.vo.TranslationInfo;
 import com.cliptripbe.feature.place.dto.PlaceDto;
-import com.cliptripbe.feature.user.domain.type.Language;
+import com.cliptripbe.feature.place.dto.response.TranslatedPlaceAddress;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public record TranslationSplitResult(
-    List<PlaceInfoWithTranslation> translatedPlaces,
+    List<TranslatedPlaceAddress> translatedPlaces,
     List<PlaceDto> untranslatedPlaces
 ) {
 
-    public List<PlaceInfoWithTranslation> mergeWith(List<TranslationInfo> newTranslations,
-        Language userLanguage) {
-
-        List<PlaceInfoWithTranslation> newlyTranslatedPlaces = IntStream.range(0,
-                untranslatedPlaces.size())
-            .mapToObj(i -> PlaceInfoWithTranslation.of(
-                untranslatedPlaces.get(i),
-                newTranslations.get(i),
-                userLanguage
-            ))
-            .toList();
-
-        List<PlaceInfoWithTranslation> allTranslatedPlaces = new ArrayList<>();
-        allTranslatedPlaces.addAll(translatedPlaces);
-        allTranslatedPlaces.addAll(newlyTranslatedPlaces);
-
+    public List<TranslatedPlaceAddress> mergeWith(List<TranslatedPlaceAddress> newTranslations) {
+        List<TranslatedPlaceAddress> allTranslatedPlaces = new ArrayList<>(this.translatedPlaces);
+        allTranslatedPlaces.addAll(newTranslations);
         return allTranslatedPlaces;
     }
 
