2068c9ff3f5dc36b37d17f1bb71eb8a4faa06708
0702Yoon <gkstkddbs99@naver.com>
2025-08-13 13:25:01 +0900

    refactor: 역할 명시

--- src/main/java/com/cliptripbe/feature/translate/dto/response/TranslationSplitResult.java
+++ src/main/java/com/cliptripbe/feature/translate/dto/response/TranslationSplitResult.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/* 번역이 된 것과 안된 것을 갖는 클래스입니다. 몰론 하나의 리스트에 번역된 정보가 없다면 번역이 되지 않은 장소로 알 수 있지만,
+안된 부분만을 번역시키고 후에 합치는 로직을 타는 것이 별로라고 생각하여 분리하여 갖도록 했습니다.
+ */
 public record TranslationSplitResult(
     List<TranslatedPlaceAddress> translatedPlaces,
     List<PlaceDto> untranslatedPlaces
