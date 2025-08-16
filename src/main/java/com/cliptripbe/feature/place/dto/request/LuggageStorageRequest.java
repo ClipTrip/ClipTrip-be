6aaa392712d1a8c4bdd35face9cd3176d274da5c
0702Yoon <gkstkddbs99@naver.com>
2025-08-17 01:12:14 +0900

    refactor: 물품보관함 요청값 예시 추가

--- src/main/java/com/cliptripbe/feature/place/dto/request/LuggageStorageRequest.java
+++ src/main/java/com/cliptripbe/feature/place/dto/request/LuggageStorageRequest.java
@@ -1,5 +1,6 @@
 package com.cliptripbe.feature.place.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
@@ -8,11 +9,13 @@ public record LuggageStorageRequest(
     @NotNull(message = "위도는 null일 수 없습니다.")
     @DecimalMin(value = "33.0", message = "위도는 33.0 이상이어야 합니다.")
     @DecimalMax(value = "38.7", message = "위도는 38.7 이하이어야 합니다.")
+    @Schema(description = "시청역 위도 (공공데이터 표준)", defaultValue = "37.5644")
     Double latitude,
 
     @NotNull(message = "경도는 null일 수 없습니다.")
     @DecimalMin(value = "124.0", message = "경도는 124.0 이상이어야 합니다.")
     @DecimalMax(value = "132.0", message = "경도는 132.0 이하이어야 합니다.")
+    @Schema(description = "시청역 경도 (공공데이터 표준)", defaultValue = "126.9771")
     Double longitude
 ) {
 
