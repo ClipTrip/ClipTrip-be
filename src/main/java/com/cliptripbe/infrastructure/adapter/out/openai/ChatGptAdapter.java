4fd0117866602a44fa55ba91686422e8c1482eed
TaetaetaE01 <gogo589@naver.com>
2025-10-17 22:11:28 +0900

    refactor: gpt 모델 변경 및 프롬포트 수정

--- src/main/java/com/cliptripbe/infrastructure/adapter/out/openai/ChatGptAdapter.java
+++ src/main/java/com/cliptripbe/infrastructure/adapter/out/openai/ChatGptAdapter.java
@@ -32,7 +32,7 @@ public class ChatGptAdapter implements AiTextProcessorPort {
             .build();
 
         ChatGptRequest request = ChatGptRequest.builder()
-            .model("gpt-4.1-mini")
+            .model("gpt-4.1-nano")
             .messages(List.of(message))
             .temperature(0.0)
             .max_tokens(350)
